a4d8f76c0e077fc7c58f6f2dcd09c715d601f77c
Cristian Navarrete <cristiannavarrete@outlook.com>
2019-04-08 18:54:32 -0400

    log de conexión y error de handshake

--- src/ftp/FTPMain.java
+++ src/ftp/FTPMain.java
@@ -2,50 +2,109 @@ package ftpserver;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.File;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class FTPMain {
-    public static void main(String[] args) throws Exception {
-        try (var listener = new ServerSocket(59898)) {
-            System.out.println("The capitalization server is running...");
-            var pool = Executors.newFixedThreadPool(20);
-            while (true) {
-                pool.execute(new Capitalizer(listener.accept()));
+
+    public class LogHandler {
+        public void StartLog() {
+            try {
+                File file = new File("log.txt");
+                file.delete();
+                file.createNewFile();
+                FileWriter fr = new FileWriter(file, true);
+                fr.write("DATETIME  EVENT   DESCRIPTION\n");
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public void writeLog(String event, String message) {
+            try {
+                DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                Date date = new Date();
+
+                File file = new File("log.txt");
+                FileWriter fr = new FileWriter(file, true);
+                fr.write(dateFormat.format(date) + "  " + event + "   " + message + "\n");
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private static class Capitalizer implements Runnable {
+    public void start() throws Exception {
+        try (var listener = new ServerSocket(59898)) {
+            System.out.println("Starting FTP Server ...");
+            LogHandler log = new LogHandler();
+            log.StartLog();
+            var pool = Executors.newFixedThreadPool(20);
+            while (true) {
+                pool.execute(new RequestHandler(listener.accept()));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        FTPMain programm = new FTPMain();
+        programm.start();
+
+    }
+
+    private class RequestHandler implements Runnable {
         private Socket socket;
 
-        Capitalizer(Socket socket) {
+        RequestHandler(Socket socket) {
             this.socket = socket;
         }
 
         @Override
         public void run() {
-            System.out.println("Connected: " + socket);
+            LogHandler log = new LogHandler();
+            String ip = socket.getInetAddress().toString();
+            log.writeLog("connection", ip.replace("/", "") + " conexión entrante");
+            int mensajes = 0;
             try {
                 var in = new Scanner(socket.getInputStream());
                 var out = new PrintWriter(socket.getOutputStream(), true);
+
+                out.println("HELLO");
                 while (in.hasNextLine()) {
                     String mensaje = in.nextLine();
                     System.out.println("Mensaje:" + mensaje);
-                    if (mensaje.equals("ls")) {
-                        out.println("Deberia Listar Archivos");
-                    } else if (mensaje.startsWith("get")) {
-                        out.println("Deberia Enviar archivo");
-                    } else if (mensaje.startsWith("put")) {
-                        out.println("Deberia subir archivo");
-                    } else if (mensaje.startsWith("delete")) {
-                        out.println("Deberia eliminar archivo");
-                    } else {
-                        out.println("Comando no reconocido");
+
+                    if (!mensaje.equals("HELLO") && mensajes == 0) {
+                        System.out.println("mensajes:" + mensajes);
+                        log.writeLog("error", ip.replace("/", "") + " conexión rechazada (invalid handshake)");
+                        out.println("Invalid Handshake message");
+                        throw new IllegalArgumentException("Error en handshake");
+                    } else if (mensajes > 0) {
+                        if (mensaje.equals("ls")) {
+                            out.println("Deberia Listar Archivos");
+                        } else if (mensaje.startsWith("get")) {
+                            out.println("Deberia Enviar archivo");
+                        } else if (mensaje.startsWith("put")) {
+                            out.println("Deberia subir archivo");
+                        } else if (mensaje.startsWith("delete")) {
+                            out.println("Deberia eliminar archivo");
+                        } else {
+                            out.println("Comando no reconocido");
+                        }
                     }
 
+                    mensajes += 1;
                     // out.println(in.nextLine().toUpperCase());
                 }
             } catch (Exception e) {
